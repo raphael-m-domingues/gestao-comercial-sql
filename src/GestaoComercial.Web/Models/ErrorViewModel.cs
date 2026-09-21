7cd580b6a1d043972a24cb711b218351ccaa6d6d
Raphael Domingues <rapha.domingues@outlook.com>
2026-09-21 16:09:22 -0300

    feat: adiciona estrutura inicial da aplicacao web

---
+++ src/GestaoComercial.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GestaoComercial.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
